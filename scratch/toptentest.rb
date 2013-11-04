666a46e9f5a96566bd15732492a5783fb92e2a08
arich <andrew.joseph.rich@gmail.com>
2013-11-04 15:04:05 -0600

    Delete toptentest.rb

--- scratch/toptentest.rb
+++
@@ -1,7 +0,0 @@
-require 'rest-client'
-
-API_KEY = "q0X0eBgwsN965QIZcDMCfDvrAqoDLRuU"
-response = RestClient.get(
-			"http://api.pearson.com/eyewitness/newyork/block.json?apikey=#{API_KEY}"
-			)
-puts response
