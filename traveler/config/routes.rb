9e9411569fac56e4350cf974734417db853bc4d3
Nahiyan Khan <nahiyan.khan@gmail.com>
2013-11-05 12:58:49 -0600

    Installed bootstrap

--- traveler/config/routes.rb
+++ traveler/config/routes.rb
@@ -1,4 +1,5 @@
 Traveler::Application.routes.draw do
+  get "welcome/index"
   resources :attractions
 
   resources :categories
@@ -9,7 +10,7 @@ Traveler::Application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  root 'welcome#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
